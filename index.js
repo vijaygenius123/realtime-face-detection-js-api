f0c37d9b78bf2ad31b6dc62b9048fa5025f91590
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-24 16:48:50 +0530

    Dawing FaceLandmarks & FaceExpressions

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ video.addEventListener('play', function () {
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
         faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
 
     }, 100)
 })
