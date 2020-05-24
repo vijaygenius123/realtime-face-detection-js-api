26a26d7da2d3b9306f7010d9611030880b4765ed
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-24 16:35:55 +0530

    Loaded Required Models And Started Video

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const video = document.getElementById('video')
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+
+]).then(function () {
+    console.log('Loaded Models')
+    startVideo()
+})
+
 function startVideo() {
     navigator.getUserMedia({ video: {} },
         stream => video.srcObject = stream,
         error => console.log(error)
     )
 }
-
-startVideo()
