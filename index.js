12ffa71455845647fb9b6483c834f00d403d01af
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-24 16:44:27 +0530

    Drawing Detections

--- index.js
+++ index.js
@@ -20,11 +20,19 @@ function startVideo() {
 
 
 video.addEventListener('play', function () {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
     setInterval(async function () {
         const detections = await faceapi.detectAllFaces(video,
             new faceapi.TinyFaceDetectorOptions())
             .withFaceLandmarks()
             .withFaceExpressions()
-        console.log(detections)
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+
     }, 100)
 })
